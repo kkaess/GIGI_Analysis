7b5f8aa565a58a2661bbf245e5e516596244579d
kkaess <kwkaess@ucdavis.edu>
2018-04-02 16:08:08 -0700

    # WARNING: head commit changed in the meantime
    Add support for working with CCfits (and cfitsio) to makefile

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -8,13 +8,20 @@
  */
 
 #include <iostream> //Import input-output library (for cout)
+#include <FITS.h> //Import CCFits, you need CCFits-devel installed
 
 using namespace std;
 //This allows me to use things like 'cout' instead of 'std::cout'
+using namespace CCfits;
+//Ditto for CCFits
 
 int main(int argc, char ** argv) {
 
 	cout << "Hello, world!" << endl; //Send "Hello, world!" to standard out, followed by newline and flush.
+
+	FITS fitsFile("TestFile.fits", RWmode::Write); //Create a blank FITS file
+
+	fitsFile.flush(); //Attempt to write the FITS file to disk (empty).
 
 	//main has type int, and should return an int. 0 indicates normal termination of main.
 	//Compiler would auto-generate this return if I didn't write it.
