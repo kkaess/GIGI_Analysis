e03284917db7ceeb87b2a99721d22daa47aec2a9
kkaess <kwkaess@ucdavis.edu>
2018-04-02 11:41:32 -0700

    Setting up my average eclipse-autogenerated c++ project structure.
    That and adding a little dummy code.

---
+++ src/analysis.cpp
@@ -0,0 +1,22 @@
+/*
+ * analysis.cpp
+ *
+ *  Created on: Apr 2, 2018
+ *      Author: kaess
+ *
+ *      Commented for Matt, who doesn't use C++
+ */
+
+#include <iostream> //Import input-output library (for cout)
+
+using namespace std;
+//This allows me to use things like 'cout' instead of 'std::cout'
+
+int main(int argc, char ** argv) {
+
+	cout << "Hello, world!" << endl; //Send "Hello, world!" to standard out, followed by newline and flush.
+
+	//main has type int, and should return an int. 0 indicates normal termination of main.
+	//Compiler would auto-generate this return if I didn't write it.
+	return 0;
+}
